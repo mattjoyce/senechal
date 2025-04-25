eeb4348a74b81a5e30ed218dd41c1c722e19192e
Matt Joyce <matt.joyce@gmail.com>
2025-04-25 09:55:04 +0000

    systemdd update script

--- systemd/update.sh
+++ systemd/update.sh
@@ -1,4 +1,4 @@
-cp /home/ubuntu/api/senechal/systemd/* /etc/systemd/system/*
+cp /home/ubuntu/api/senechal/systemd/* /etc/systemd/system/
 sudo systemctl daemon-reload
 
 sudo systemctl restart senechal_etl_withings.timer
@@ -7,4 +7,6 @@ sudo systemctl enable senechal_etl_withings.timer
 sudo systemctl restart senechal_etl_garmin.timer
 sudo systemctl enable senechal_etl_garmin.timer
 
-sudo systemctl restart senechal_service
+sudo systemctl restart garmindb-sync.service
+sudo systemctl restart senechal-discord.service
+sudo systemctl restart senechal.service
